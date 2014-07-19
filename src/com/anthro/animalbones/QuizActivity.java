a2c6b3e363b493464609402548d8dc029759dc0d
William Kerrigan <Deadleg@gmail.com>
2014-07-19 21:09:34 +1200

    Updated to support imageFragment changes

--- src/com/anthro/animalbones/QuizActivity.java
+++ src/com/anthro/animalbones/QuizActivity.java
@@ -2,25 +2,37 @@ package com.anthro.animalbones;
 
 import java.util.Arrays;
 import java.util.List;
+
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.app.ListFragment;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class QuizActivity extends Activity implements QuizListFragment.OnListItemSelectedListener, QuizImageFragment.QuizImageListener {
 	
 	QuizImageFragment imageFragment;
 	final static String FRAGMENT_IMAGE = "com.anthro.animalbones.FRAGMENT_IMAGE";
+	public Quiz quiz;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_quiz);
 		
+		// Initialize quiz
+		quiz = new Quiz(new CircleAnimal(this));
+		
 		// Initialize fragments
 		imageFragment = new QuizImageFragment();
 		
+		// Setup fragments
 		FragmentTransaction transaction = getFragmentManager().beginTransaction();
 		if (savedInstanceState == null) {
 			transaction.add(R.id.fragment_quiz_container, imageFragment, FRAGMENT_IMAGE);
@@ -39,6 +51,7 @@ public class QuizActivity extends Activity implements QuizListFragment.OnListIte
         		if (j != i && fragments.get(j) != null) {
         			transaction.hide(fragments.get(j));
         		}
+        		
         	}
         	transaction.show(fragments.get(i));	
         }
@@ -74,8 +87,73 @@ public class QuizActivity extends Activity implements QuizListFragment.OnListIte
 
 	@Override
 	public void answerSelected(String answer) {
-		// TODO Auto-generated method stub
+		TextView textView = (TextView) imageFragment.getView().findViewById(R.id.textViewAnswerFeedback);
+		if (quiz.isCorrect(answer)) {
+			setNextQuestion();
+			setImages();
+			textView.setText("Correct!");
+		} else {
+			textView.setText("Wrong!");
+		}
 		
 	}
+	
+	public void setNextQuestion() {
+		quiz.getNextQuestion();
+			
+		List<String> newAnswers = quiz.getAnswerList();
+			
+		// Get listView.
+		ListFragment list = (ListFragment) getFragmentManager().findFragmentById(R.id.fragment_quiz_list);
+		ListView listView = (ListView)list.getListView();
+		
+		// Get the ArrayAdapter.
+		@SuppressWarnings("unchecked")
+		ArrayAdapter<String> adapter = (ArrayAdapter<String>)listView.getAdapter();
+		
+		// Set the new list in the array adapter.
+		adapter.clear();
+		adapter.addAll(newAnswers);
+		adapter.notifyDataSetChanged();
+	}
+	
+	private void setImages() {
+		ImageView foregroundView = (ImageView) imageFragment.getView().findViewById(R.id.imageViewForeground);
+		ImageView backgroundView = (ImageView) imageFragment.getView().findViewById(R.id.imageViewBackground);
+		
+		displayForegroundImage(foregroundView);
+		displayBackgroundImage(backgroundView);
+	}
+	
+	public void displayForegroundImage(ImageView iView) {
+		Bitmap foreground = quiz.currentQuestion.imageForeground;
+		iView.setImageBitmap(foreground);
+	}
+	
+	public void displayBackgroundImage(ImageView iView) {
+		Bitmap background = quiz.currentQuestion.imageBackground;
+		iView.setImageBitmap(background);
+	}
+
+	@Override
+	public void setAnswerList() {
+		// TODO Check where question already exists or is new
+		setNextQuestion();
+	}
+
+	@Override
+	public void imagePressed(int colour) {
+		if (!quiz.currentQuestion.isOptionQuestion) {
+			if (quiz.animal.boneColours.get(quiz.currentQuestion.correctAnswer) == colour) {
+				answerSelected(quiz.currentQuestion.correctAnswer);
+			} else {
+				answerSelected("");
+			}
+		} else {
+			// Do nothing.
+		}
+		
+	}
+
 
 }
